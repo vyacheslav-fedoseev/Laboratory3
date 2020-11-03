5a5690869a419380b6baa2234be79d37d333f026
vyacheslav_fedoseev <slava140901@gmail.com>
2020-11-03 13:35:58 +0300

    Changed: Gorner scheme. Added: referenceMenu, its work logic; third column with information isItPalindrom?

--- src/bsu/rfe/java/group7/lab3/Fedoseev/varB6/GornerTableModel.java
+++ src/bsu/rfe/java/group7/lab3/Fedoseev/varB6/GornerTableModel.java
@@ -25,7 +25,7 @@ public class GornerTableModel extends AbstractTableModel {
     }
     public int getColumnCount() {
 // В данной модели два столбца
-        return 2;
+        return 3;
     }
     public int getRowCount() {
 // Вычислить количество точек между началом и концом отрезка
@@ -35,17 +35,27 @@ public class GornerTableModel extends AbstractTableModel {
     public Object getValueAt(int row, int col) {
 // Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
         double x = from + step*row;
+        // Схема Горнера
+        double result = coefficients[0];
+        for (int i = 1; i < coefficients.length; ++i)
+            result = result * x + coefficients[i];
 
         switch(col) {
             // Если запрашивается значение 1-го столбца, то это X
-            case 0: return x;
+            case 0: {
+                return x;
+            }
             // Если запрашивается значение 2-го столбца, то это значение
             // многочлена
             case 1: {
-                double result = coefficients[coefficients.length - 1];
-                for(int i = coefficients.length - 1; i > 0; --i)
-                    result = result * x + coefficients[i - 1];
                 return result;
+            }
+            // Если запрашивается значение 3-го столбца, то проверяем на палиндром
+            case 2: {
+                StringBuffer numberStr = new StringBuffer(String.valueOf((int)result));
+                if(numberStr.toString().compareTo(numberStr.reverse().toString()) == 0)
+                   return 1.0;
+                else return 0.0;
             }
             default: return 0.0;
         }
@@ -56,9 +66,14 @@ public class GornerTableModel extends AbstractTableModel {
             case 0:
 // Название 1-го столбца
                 return "Значение X";
-            default:
+            case 1:
 // Название 2-го столбца
                 return "Значение многочлена";
+            //case 2:
+                // Название 3-го столбца
+              //  return "Целая часть палиндром?";
+            default:
+                return "Smth";
         }
     }
     public Class<?> getColumnClass(int col) {
